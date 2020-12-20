7c5cfb5a7b7e6ed87f54c670e5bf721a10a32654
Lewis <casibila@gmail.com>
2020-12-20 20:11:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+import os
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$remind'):
+        await message.channel.send('What would you like your reminder to be?')
+
+        discord.on_message(message):
+          
+
+client.run(os.getenv('TOKEN'))
